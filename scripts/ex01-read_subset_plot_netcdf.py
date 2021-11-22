6f903e3c348d9c8b4002d26b0ddf2fbcee023668
Shanto Khandoker <shantokhandoker0@gmail.com>
2021-11-22 12:08:34 +0600

    init

---
+++ scripts/ex01-read_subset_plot_netcdf.py
@@ -0,0 +1,27 @@
+#       Open and process netcdf4 data using xarray
+
+import xarray as xr
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import numpy as np
+
+#read data
+path = r"H:\Python project 2021\climate_data_analysis_with_python\data\HadISST_sst.nc"
+nc_file = xr.open_dataset(path)
+
+# plot a single month
+nc_file['sst'][-1].plot(cmap = 'jet',vmax = 35,vmin = -10)
+
+# making colimatologies by averaging all the month
+nc_clim = nc_file['sst'].mean('time',keep_attrs = True)
+
+nc_clim.plot(cmap = 'jet',vmax = 35,vmin = -10)
+nc_clim.attrs['units'] = 'degree C'
+
+fig = plt.figure(figsize=[12,5])
+ax = fig.add_subplot(111, projection=ccrs.PlateCarree(central_longitude=180))
+
+nc_clim.plot.contourf(ax = ax,levels = np.arange(-10,36,2),extend = 'max', transform = ccrs.PlateCarree(),cbar_kwargs = {'label': nc_clim.units},cmap = 'jet')
+
+ax.coastlines()
+plt.show()
